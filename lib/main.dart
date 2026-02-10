ab8464387e24132761c558aec04322c63dd9a0c8
AM-AN <edchaamant@email.com>
2026-02-10 11:45:12 -1200

    Firebase Homework

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_homework/firebase_options.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Firebase Test')),
+        body: Center(child: Text('Firebase Connected!')),
+      ),
+    );
+  }
+}
